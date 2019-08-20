ebcf0be81099dcc5d39470d2721977e5606ac745
bharath <bhrth.dsra1@gmail.com>
2019-08-20 14:51:21 +0800

    Add the tags as Name

--- main.tf
+++ main.tf
@@ -7,9 +7,13 @@ resource "aws_instance" "bharths_ec2" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   availability_zone = "us-east-2a"
-  count = 0
+  count = 1
   user_data = <<-EOF
          #!/bin/bash
          sudo service apache2 restart
   EOF
+
+  tags {
+    Name = "terraform-example"
+  }
 }
