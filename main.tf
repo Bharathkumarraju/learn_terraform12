33f647c64382417a401329124a49a1baefd191df
bharath <bhrth.dsra1@gmail.com>
2019-08-20 15:29:39 +0800

    WOWWW ...now we have working webserver running in AWS Cloud

--- main.tf
+++ main.tf
@@ -8,12 +8,24 @@ resource "aws_instance" "bharths_ec2" {
   instance_type = "t2.micro"
   availability_zone = "us-east-2a"
   count = 1
+  vpc_security_group_ids = [aws_security_group.bharths_sg.id]
   user_data = <<-EOF
          #!/bin/bash
-         sudo service apache2 restart
+         echo "Hello, World!" > index.html
+         nohup busybox httpd -f -p 8080 &
   EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "bharths_sg" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port = 8080
+    protocol = "tcp"
+    to_port = 8080
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
