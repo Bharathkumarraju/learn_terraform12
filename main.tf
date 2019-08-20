430c4dc0f0116e10367042397d141e3294a1cbf3
bharath <bhrth.dsra1@gmail.com>
2019-08-20 13:11:14 +0800

    Add the smaple webserver installation with terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
 resource "aws_instance" "bharths_ec2" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
-  availability_zone = "us-east-1a"
+  availability_zone = "us-east-2a"
   user_data = <<-EOF
          #!/bin/bash
          sudo service apache2 restart
