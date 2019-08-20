79aff5907da34c338e0e1399774e73b4a57b2236
bharath <bhrth.dsra1@gmail.com>
2019-08-20 14:47:06 +0800

    First Terraform test using AWS provider

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "aws_instance" "bharths_ec2" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
   availability_zone = "us-east-2a"
+  count = 0
   user_data = <<-EOF
          #!/bin/bash
          sudo service apache2 restart
