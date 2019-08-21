ad63491541a5009b6adb10440978e58b85ab0fa0
bharath <bhrth.dsra1@gmail.com>
2019-08-21 22:03:08 +0800

    Add the terraform backend

--- main.tf
+++ main.tf
@@ -87,6 +87,16 @@ provider "aws" {
   region = "us-east-2"
 }
 
+terraform {
+  backend "s3" {
+    bucket = "bharaths-terraform-up-and-running"
+    key = "global/s3/terraform.tfstate"
+    region = "us-east-2"
+    dynamodb_table = "bharaths-terraform-up-and-running-locks"
+    encrypt = true
+  }
+}
+
 /*resource "aws_instance" "bharths_ec2" {
   ami = "ami-0c55b159cbfafe1f0"
   instance_type = "t2.micro"
---
+++ remote_state/main.tf
@@ -0,0 +1,34 @@
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_s3_bucket" "bharath_tf_state_store" {
+  bucket = "bharaths-terraform-up-and-running"
+
+  # Prevent Accedental Delete of this bucket
+  lifecycle {
+    prevent_destroy = true
+  }
+  # Enables versioning
+  versioning {
+    enabled = true
+  }
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "bharaths-terraform-locks" {
+  hash_key = "LockID"
+  name = "bharaths-terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
