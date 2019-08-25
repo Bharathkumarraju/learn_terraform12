6c838a21f4700af3a5920c40a77c2da8e6c69443
bharath <bhrth.dsra1@gmail.com>
2019-08-25 17:35:40 +0800

    How to add iam policy and iam policy attachments

--- Terraform_Folder_Structure/loops/iam/main.tf
+++ Terraform_Folder_Structure/loops/iam/main.tf
@@ -7,3 +7,25 @@ resource "aws_iam_user" "bharaths_iam_users" {
   count = length(var.user_names)
   name = var.user_names[count.index]
 }
+
+data "aws_iam_policy_document" "ec2_readonly" {
+  statement {
+    effect = "Allow"
+    actions = ["ec2:Describe*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "ec2_readonly" {
+  name = "ec2-read-only"
+  policy = data.aws_iam_policy_document.ec2_readonly.json
+}
+
+resource "aws_iam_policy_attachment" "ec2_access" {
+  name = "ec2-policy-attachment"
+  count = length(var.user_names)
+  users = element(aws_iam_user.bharaths_iam_users[*].name, count.index)
+  # Above one also can be written as below
+  # users = aws_iam_user.bharaths_iam_users[count.index].name
+  policy_arn = aws_iam_policy.ec2_readonly.arn
+}
