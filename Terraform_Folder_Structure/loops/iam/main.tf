8f737de9d51931924f8bae9923ac3113dac43629
bharath <bhrth.dsra1@gmail.com>
2019-08-25 17:44:09 +0800

    How to use element fucntion and array lookups

--- Terraform_Folder_Structure/loops/iam/main.tf
+++ Terraform_Folder_Structure/loops/iam/main.tf
@@ -21,11 +21,17 @@ resource "aws_iam_policy" "ec2_readonly" {
   policy = data.aws_iam_policy_document.ec2_readonly.json
 }
 
-resource "aws_iam_policy_attachment" "ec2_access" {
-  name = "ec2-policy-attachment"
+/*
+The difference between element and array lookups is what happens if you try to access an index that is out of bounds.
+For example, if you tried look up index 4 in an array with only 3 items,
+the array lookup would give you an error,
+whereas the element function will loop around using a standard mod algorithm, returning the item at index 1.
+*/
+
+resource "aws_iam_user_policy_attachment" "ec2_access" {
   count = length(var.user_names)
-  users = element(aws_iam_user.bharaths_iam_users[*].name, count.index)
-  # Above one also can be written as below
-  # users = aws_iam_user.bharaths_iam_users[count.index].name
   policy_arn = aws_iam_policy.ec2_readonly.arn
+  user = element(aws_iam_user.bharaths_iam_users[*].name, count.index)
+  # Above one can also be written as
+  # user = aws_iam_user.bharaths_iam_users[count.index].name
 }
