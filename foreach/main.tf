1e0c669a5a6f87fd48f8a370791d3c2300c0835c
Bharath <bharath@hooq.tv>
2019-09-23 11:56:06 +0800

    how to use for_each with terraform:

---
+++ foreach/main.tf
@@ -0,0 +1,11 @@
+
+provider "aws" {
+  profile = "default"
+  region = "us-east-2"
+}
+
+resource "aws_iam_user" "bharth_user" {
+  for_each = toset(var.user_names)
+  name = each.value
+}
+
---
+++ foreach/variables.tf
@@ -0,0 +1,5 @@
+variable "user_names" {
+  description = "Create IAM users"
+  type = list(string)
+  default = ["raju11", "raju12", "raju13"]
+}
